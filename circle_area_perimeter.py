b7e5f805a5ec7e3d54850acf6b43dc224b88f9a3
Ryan-Nguyen-7 <ryan.nguyen@mths.ca>
2020-11-25 11:30:48 +0000

    added circle_area_perimeter

---
+++ circle_area_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Nguyen
+# Created on November 2020
+# This program calculates area and perimeter of a circle
+
+
+def main():
+    # this function calculates A+P
+    import math
+    print("If a circle has a radius of 15cm")
+    print("")
+    print("Area is {}cm^2".format(math.pi*15**2))
+    print("Perimeter is {}cm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
